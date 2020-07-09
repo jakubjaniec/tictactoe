48beea513710bf58b5f1d278c02caabd750bcb08
jakubjaniec <jakubjaniec@aol.com>
2020-07-09 17:03:27 +0200

    Add basic winner checker

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,44 @@ class _TicTacToeState extends State<TicTacToe> {
     setState(() {
       xMove = !xMove;
     });
+    checkWinner();
+  }
+
+  void checkWinner() {
+    for (int i = 0; i < 3; i++) {
+      // horizontal check
+      if (movesList[i][0] == 'X' &&
+          movesList[i][1] == 'X' &&
+          movesList[i][2] == 'X')
+        print('X WON!');
+      else if (movesList[i][0] == 'O' &&
+          movesList[i][1] == 'O' &&
+          movesList[i][2] == 'O') print('O WON!');
+
+      // vertical check
+      if (movesList[0][i] == 'X' &&
+          movesList[1][i] == 'X' &&
+          movesList[2][i] == 'X')
+        print('X WON!');
+      else if (movesList[0][i] == 'O' &&
+          movesList[1][i] == 'O' &&
+          movesList[2][i] == 'O') print('O WON!');
+
+      // cross check
+      if ((movesList[0][0] == 'X' &&
+              movesList[1][1] == 'X' &&
+              movesList[2][2] == 'X') ||
+          (movesList[0][2] == 'X' &&
+              movesList[1][1] == 'X' &&
+              movesList[2][0] == 'X'))
+        print('X WON!');
+      else if ((movesList[0][0] == 'O' &&
+              movesList[1][1] == 'O' &&
+              movesList[2][2] == 'O') ||
+          (movesList[0][2] == 'O' &&
+              movesList[1][1] == 'O' &&
+              movesList[2][0] == 'O')) print('O WON!');
+    }
   }
 
   @override
@@ -40,41 +78,53 @@ class _TicTacToeState extends State<TicTacToe> {
           Expanded(
             child: Container(
               color: Colors.grey[600],
-              child: Center(
-                child: Container(
-                  width: 400,
-                  height: 400,
-                  color: Colors.grey[500],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Field(saveChoice, movesList, 0, 0),
-                          Field(saveChoice, movesList, 0, 1),
-                          Field(saveChoice, movesList, 0, 2),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Field(saveChoice, movesList, 1, 0),
-                          Field(saveChoice, movesList, 1, 1),
-                          Field(saveChoice, movesList, 1, 2),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Field(saveChoice, movesList, 2, 0),
-                          Field(saveChoice, movesList, 2, 1),
-                          Field(saveChoice, movesList, 2, 2),
-                        ],
-                      ),
-                    ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 60),
+                    child: Text(
+                      xMove ? 'X MOVE' : 'O MOVE',
+                      style: TextStyle(fontSize: 32.0, color: Colors.grey[300]),
+                    ),
                   ),
-                ),
+                  Center(
+                    child: Container(
+                      width: 400,
+                      height: 400,
+                      color: Colors.grey[500],
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Field(saveChoice, movesList, 0, 0),
+                              Field(saveChoice, movesList, 0, 1),
+                              Field(saveChoice, movesList, 0, 2),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Field(saveChoice, movesList, 1, 0),
+                              Field(saveChoice, movesList, 1, 1),
+                              Field(saveChoice, movesList, 1, 2),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Field(saveChoice, movesList, 2, 0),
+                              Field(saveChoice, movesList, 2, 1),
+                              Field(saveChoice, movesList, 2, 2),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
